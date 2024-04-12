28cba69badadca678208c2e2a2bf857ab5255ae2
Osbertt-19 <minhtetnaing25mhn@gmail.com>
2024-04-12 17:57:05 +0630

    changed question order

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,12 @@
 questions = [
     "How do you write comments in Python?",
-    "Why might you use comments in your Python code?",
+    "How are lists different from tuples?",
     "What are the basic data types in Python?",
+    "Why might you use comments in your Python code?",
     "How do you assign a value to a variable in Python?",
     "Why is indentation crucial in Python code?",
     "How do you take user input in Python?",
     "How do you access elements in a list?",
-    "How are lists different from tuples?",
     "How do you access values in a dictionary?",
     "Why are functions beneficial in Python?",
     "How to define a function in Python?",
@@ -17,16 +17,16 @@ questions = [
 # some are incorrect
 sample_answers = [
     "# This is a single-line comment",
-    "To explain complex logic or improve code readability",
-    "numbers,text,booleans",
-    "name = value",
-    "Defines code blocks and execution flow",
-    "print()",
-    "list_name[index]",
     "Lists are ordered, changeable, and use square brackets. Tuples are similar but immutable (unchangeable) and use parentheses.",
-    "dictionary_name['key']",
-    "Break down complex tasks, improve code reusability, and enhance readability.",
-    "with the def keyword",
+    "int, boolean",
+    # "To explain complex logic or improve code readability",
+    # "name = value",
+    # "Defines code blocks and execution flow",
+    # "print()",
+    # "list_name[index]",
+    # "dictionary_name['key']",
+    # "Break down complex tasks, improve code reusability, and enhance readability.",
+    # "with the def keyword",
 ]
 
 answers = []
